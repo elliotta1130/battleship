a2577793256c31c3c416180cfca45a9d6f6e8678
Elliott <elliotta1130@yahoo.com>
2015-04-07 04:19:43 +0000

    stevies additions

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 from random import randint
 
-board = []
+class Board:
+    """This represents a board of the game"""
+    def __init__(self, size):
+        self.board = []
+        for x in range(size):
+            self.board.append(["O"] * size)
+        self.ship_row = randint(0, len(self.board) - 1)
+        self.ship_col = randint(0, len(self.board[0]) - 1)
+    def print_board(self):
+        for row in self.board:
+            print " ".join(row)
+    def print_ship_location(self):
+        print self.ship_row, self.ship_col
+    def is_hit(self, row, col):
+        # TODO: complete this
+        print 'todo'
+    
+    
+print 'testing new boards'
+b1 = Board(5)
+b2 = Board(5)
 
-for x in range(5):
-    board.append(["O"] * 5)
+b1.print_ship_location()
+b2.print_ship_location()
 
-def print_board(board):
-    for row in board:
-        print " ".join(row)
 
 print "Let's play Battleship!"
-print_board(board)
-
-def random_row(board):
-    return randint(0, len(board) - 1)
-
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-
 
 for turn in range(4):
+    whosturn = turn % 2
     guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Col:"))
+    board = None
+    if whosturn == 1:
+        board = b1
+    else:
+        board = b2
 
+    board.is_hit(guess_row, guess_col)
+    
+    # TODO: move this code to is_hit
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
         break
@@ -43,4 +58,4 @@ for turn in range(4):
                 board[guess_row][guess_col] = "X"
         turn = turn + 0   
         print "turn {}".format(turn + 1)
-        print_board(board)
+        board.print_board()
